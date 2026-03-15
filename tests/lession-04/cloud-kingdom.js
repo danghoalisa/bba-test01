4518a94b44ec1dc0777fcef73c0aca04c3f3df82
test 01 - Hoa Dang <danghoa131091@gmail.com>
2026-03-16 00:22:44 +0700

    feat: add solution for challenge 03

---
+++ tests/lession-04/cloud-kingdom.js
@@ -0,0 +1,13 @@
+
+let playerName ='Mario';
+
+let currentLives = 3;
+
+const levelOne= 25;
+const levelTwo = 30;
+const levelThree = 45;
+
+const sum= levelOne + levelTwo + levelThree
+const gttb = sum/3;
+const coinConLai = sum % 3;
+console.log (coinConLai);
