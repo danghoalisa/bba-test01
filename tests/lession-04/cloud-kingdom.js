0c9a1dbef3c18e9618070e8bbe2181fbbcad579b
test 01 - Hoa Dang <danghoa131091@gmail.com>
2026-03-16 12:04:48 +0700

    feat: reformat code

--- tests/lession-04/cloud-kingdom.js
+++ tests/lession-04/cloud-kingdom.js
@@ -1,16 +1,18 @@
 
-let playerName ='Mario';
+let playerName = 'Mario';
 
 let currentLives = 3;
 
-const levelOne= 25;
+const levelOne = 25;
 const levelTwo = 30;
 const levelThree = 45;
 
-const sum= levelOne + levelTwo + levelThree
-const gttb = sum/3;
+const sum = levelOne + levelTwo + levelThree
+const gttb = sum / 3;
 const coinConLai = sum % 3;
-console.log (coinConLai);
+console.log(coinConLai);
+
+
 let powerUp = "flower";
 let effect = '';
 
